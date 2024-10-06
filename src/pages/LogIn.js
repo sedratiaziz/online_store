11b3bc82bc0d786ddf854d28aa6ff917dfc7926b
sedratiaziz <87585184+sedratiaziz@users.noreply.github.com>
2024-10-06 23:06:22 +0300

    v3.5 added login page but not working yet

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styles from '../assets/css/LogIn.module.css'; // Import the CSS module
 
 export default function LogIn() {
-  return (
-    <div className='h101'>
-        <h1 className='h111'>Log in</h1>
-    </div>
-  )
+    return (
+        <section className={`py-5 d-flex align-items-center ${styles.LogIn__container}`} id="sec1">              
+            <div className={styles.LogIn__contact_box}>
+                <div className={styles.LogIn__left}><h3 className={styles.LogIn__lefth3}>Bazaria™</h3></div>
+                <div className={styles.LogIn__right}>
+                    <h2 className={styles.LogIn__h2}>سجّل وعيش تجربة تسوّق فريدة</h2>
+                    <form data-netlify="true">
+                        <input
+                            type="text"
+                            name="name"
+                            className={styles.LogIn__field}
+                            placeholder="الإسم الكريم"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="email"
+                            className={styles.LogIn__field}
+                            placeholder="البريد الإلكتروني ( الإيميل )"
+                            required
+                        />
+                        <input
+                            type="text"
+                            name="message"
+                            className={`${styles.LogIn__field} ${styles.LogIn__f1}`}
+                            placeholder="شنو المنتجات اللي تفضلها؟ "
+                            required
+                        />
+                        <button className={`${styles.LogIn__btn1} mt-3`}>تسجيل الدخول</button>
+                        <div className={`${styles.LogIn__rowDiv} mt-3`}>
+                          <button className={`${styles.LogIn__btn2} mt-3`}>إنشاء حساب بازاريا</button>
+                          <button className={`${styles.LogIn__btn2} mt-3`}>نسيت كلمة المرور؟</button>
+                        </div>                        
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
 }
