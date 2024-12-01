711a26c567fd0d06e0f717f45b9f5aa161d9a1a2
sedratiaziz <azizsedrati72@gmail.com>
2024-12-02 00:40:38 +0300

    v4.3 fixed error while building

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
